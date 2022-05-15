3916d17e5dea6ab60024560089ed714738f0aa8d
ndurivin <ndurivo@gmail.com>
2022-05-15 05:36:19 +0300

    Start of the javascript script in targetting the form

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+
+// declare the variables
+
+function submitResults(){
+    var totalQuiz = 5;
+    var initialScore = 0;
+
+    // Getting data from the game player (User)
+
+    var q1 = document.forms["quizForm"]["q1"].value;
+    var q2 = document.forms["quizForm"]["q2"].value;
+    var q3 = document.forms["quizForm"]["q3"].value;
+    var q4 = document.forms["quizForm"]["q4"].value;
+    var q5 = document.forms["quizForm"]["q5"].value;
+
+    alert(q1);
+}
